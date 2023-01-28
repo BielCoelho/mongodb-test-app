d7a7f6a37ba91d71e09e7cae96a30867c5afbd52
Gabriel Coelho <gabrielcuei98@gmail.com>
2023-01-28 17:13:19 +0000

    deploy

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const connectionString = "mongodb://localhost:27017";
+
+async function init() {
+  const client = new MongoClient(connectionString, {
+    useUnifiedTopology: true,
+  });
+
+  await client.connect();
+
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    const db = await client.db("adoption");
+    const collection = db.collection("pets");
+
+    const pets = await collection
+      .find(
+        {
+          $text: { $search: req.query.search },
+        },
+        {
+          _id: false,
+        }
+      )
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10)
+      .toArray();
+
+    res.status(200).json(pets).end();
+  });
+
+  const PORT = 3000;
+  app.use(express.static("./static"));
+  app.listen(PORT, () => {});
+  console.log(`running on: http://localhost:${PORT}`);
+}
+init();
